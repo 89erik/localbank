929f22188be40c292a6b175298e3855b3e20c2c9
Erik Lothe <eriklothe@gmail.com>
2018-07-24 11:36:36 +0200

    forbedringer i deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
 scp *.py pi2:/var/www/api/
 scp localbank.wsgi pi2:/var/www/api/
 
 cd frontend
 npm run build
+ssh pi2 "mkdir -p /var/www/api/frontend; rm -rf /var/www/api/frontend/*"
 scp -r build/* pi2:/var/www/api/frontend/
