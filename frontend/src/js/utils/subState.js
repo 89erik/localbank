15a338f59a81a5683b5d43f7028ffa93448668f2
Erik Lothe <eriklothe@gmail.com>
2018-07-24 17:05:22 +0200

    fjerner ubrukt fil

--- frontend/src/js/utils/subState.js
+++
@@ -1,9 +0,0 @@
-export default name => ({
-    get: {
-        request: {type: id("GET", "REQUEST", name)},
-        success: {type: id("GET", "SUCCESS", name)},
-        failure: {type: id("GET", "FAILURE", name)}
-    }
-});
-
-const id = (verb, name, stage) => `${name}_${stage}_${verb}`;
