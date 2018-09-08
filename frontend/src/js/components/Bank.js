bfbb76e21e35e5209a865772205be21ab3964e8b
Erik Lothe <eriklothe@gmail.com>
2018-09-08 17:36:13 +0200

    footer med link til github

--- frontend/src/js/components/Bank.js
+++ frontend/src/js/components/Bank.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Transaksjoner from './Transaksjoner'
 import NyTransaksjon from './NyTransaksjon'
+import '../../style/footer.css'
 
 class Bank extends Component {
 
@@ -10,6 +11,12 @@ class Bank extends Component {
           <div className="bank">
             <NyTransaksjon />
             <Transaksjoner />
+            <footer>
+                <span>localbank/{this.props.match.params.bankId}</span>
+                <a href="https://github.com/89erik/localbank" target="_blank" rel="noopener noreferrer" title="Kildekode">
+                    <img src="GitHub-Mark-32px.png" alt="github"/>
+                </a>
+            </footer>
           </div>
         );
     }
