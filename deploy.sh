f01bbed29ec56daeba9dff27e168b902634a61b0
Erik Lothe <eriklothe@gmail.com>
2018-09-23 17:00:22 +0200

    flytter backend til egen mappe

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 cd "$(dirname "$0")"
 
+cd backend
 scp *.py localbank.wsgi requirements.txt pi2:/var/www/api/
 ssh pi2 "ls /var/www/api/venv &> /dev/null || virtualenv /var/www/api/venv"
 ssh pi2 "cd /var/www/api && . venv/bin/activate && pip install -r requirements.txt"
 
-cd frontend
+cd ../frontend
 npm run build
 ssh pi2 "mkdir -p /var/www/api/frontend; rm -rf /var/www/api/frontend/*"
 scp -r build/* pi2:/var/www/api/frontend/
