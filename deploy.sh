f8cbb3631f0650cb72f4bc0b8d2c1b4eeedc21cb
Erik Lothe <eriklothe@gmail.com>
2018-09-23 17:53:03 +0200

    endrer filstruktur på server

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 cd "$(dirname "$0")"
 
+ssh pi2 "mkdir -p /var/www/localbank/backend && mkdir -p /var/www/localbank/frontend"
+
 cd backend
-scp *.py localbank.wsgi requirements.txt pi2:/var/www/api/
-ssh pi2 "ls /var/www/api/venv &> /dev/null || virtualenv /var/www/api/venv"
-ssh pi2 "cd /var/www/api && . venv/bin/activate && pip install -r requirements.txt"
+scp *.py localbank.wsgi requirements.txt pi2:/var/www/localbank/backend/
+ssh pi2 "ls /var/www/localbank/backend/venv &> /dev/null || virtualenv /var/www/localbank/backend/venv"
+ssh pi2 "cd /var/www/localbank/backend && . venv/bin/activate && pip install -r requirements.txt"
 
 cd ../frontend
 npm run build
-ssh pi2 "mkdir -p /var/www/api/frontend; rm -rf /var/www/api/frontend/*"
-scp -r build/* pi2:/var/www/api/frontend/
+ssh pi2 "rm -rf /var/www/localbank/frontend/*"
+scp -r build/* pi2:/var/www/localbank/frontend/
